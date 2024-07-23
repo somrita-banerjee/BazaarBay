a9ab5e5b73af92e978596e95edd689be46e44f83
somrita-banerjee <somritabanerjee126@gmail.com>
2024-07-23 21:50:25 +0530

    generated basic resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ProductModule, OrderModule],
 })
 export class AppModule {}
